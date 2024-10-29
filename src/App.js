a64b63a57e3e8e5767ab13732da5694db1d209de
Ömer Çıkan <omercikan36@gmail.com>
2024-10-29 19:23:57 +0300

    Moved from class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,86 +1,60 @@
 import Header from "./components/Header";
 import Search from "./components/Search";
-import React, { Component } from 'react'
+import React, {useEffect, useState } from 'react'
 import UserList from "./components/UserList";
 import Alert from "./components/Alert";
-// import Error from "./components/Error";
 
-export class App extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-      loading: false,
-      user: [],
-      otherUser: [],
-      error: null,
-      // info: null
-    }
-  }
+function App() {
+  const [user, setUser] = useState([]);
+  const [otherUser, setOtherUser] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  getUsersData = (user) => {
-    this.setState({loading: true})
+  const getUsersData = (user) => {
+    setLoading(true)
 
-    setTimeout(() => {
-      fetch("https://api.github.com/search/users?q=" + user)
-      .then((response) => response.json())
-      .then((data) => this.setState({user: data.items, loading: false}))
+  setTimeout(() => {
+    fetch("https://api.github.com/search/users?q=" + user)
+    .then((response) => response.json())
+    .then((data) => setUser(data), setLoading(false))
+    .catch((error) => setError(error), setLoading(false))
     }, 1000);
-
   }
 
-  getUserOtherData = (user) => {
+  const getUserOtherData = (user) => {
     fetch("https://api.github.com/users/" + user)
-    .then((response) => {
-      if(!response.ok) {
-        // this.displayError("Kullanıcı bulunamadı!")
-        // return null;
-      }
-      return response.json() 
-    })
-    .then((data) => this.setState({otherUser: data}))
-    // .catch((err) => this.displayError(err))
+    .then((response) => response.json())
+    .then((data) => setOtherUser(data))
+    .catch((error) => setError(error));
   }
 
-  /*displayError = (message) => {
-    this.setState({info: {err: message}})
+  useEffect(() => {
+    if(error) {
+      const timer = setTimeout(() => {
+        setError(null)
+      }, 3000);
+      return () => clearTimeout(timer)
+    }
+  }, [error])
 
-    setTimeout(() => {
-      this.setState({info: null})
-    }, 3000);
-  }*/
+  return (
+    <main className="parent-element">
+      <Header/> 
 
-  displayAlert = (msg) => {
-    this.setState({error: {msg, msg}})
+      <Search 
+      searchUser={getUsersData} 
+      otherUser={getUserOtherData} 
+      loading={loading} 
+      alert={setError}/>
 
-    setTimeout(() => {
-      this.setState({error: null})
-    }, 3000);
-  }
-  
-  render() {
-    return (
-      <main className="parent-element">
-        <Header/>
-        
-        <Search 
-        searchUser={this.getUsersData} 
-        otherUser={this.getUserOtherData} 
-        loading={this.state.loading} 
-        alert={this.displayAlert} 
-        error={this.displayError} 
-        />
+      <Alert showAlert={error}/>
 
-        <Alert showAlert={this.state.error}/>
-        {/* <Error showError={this.state.info}/> */}
-
-        <UserList 
-        users={this.state.user} 
-        otherUser={this.state.otherUser} 
-        loading={this.state.loading}/>
-      </main>
-      );
-  }
+      <UserList 
+      users={user} 
+      otherUser={otherUser} 
+      loading={loading}/>
+    </main>
+  )
 }
 
 export default App
--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -7,7 +7,7 @@ export class Alert extends Component {
         <div className="alert alert-danger d-flex align-items-center mt-3 w-100" role="alert">
             <div>
               <i className="bi bi-exclamation-triangle-fill me-2"></i>
-              {this.props.showAlert.msg}
+              {this.props.showAlert}
             </div>
         </div>
       )
